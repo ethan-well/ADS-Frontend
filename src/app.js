dc02b2c0daf44baf6ac02131c4b86c0a4de03481
ItsWewin <wewin11235@163.com>
2020-01-17 23:53:40 +0800

    use material ui

--- src/app.js
+++ src/app.js
@@ -1,41 +1,10 @@
-import React, { useState, useEffect, Fragment } from 'react';
-import useHackerNewsApi from './customHook';
+import React, { Fragment } from 'react';
+import SignIn from './login/login.js';
 
 function App() {
-  const [query, setQuery] = useState('redux');
-  const [{ data, isLoading, isError }, doFetch] = useHackerNewsApi(
-    'https://hn.algolia.com/api/v1/search?query=redux',
-    { hits: [] },
-  );
-
   return (
     <Fragment>
-      <form onSubmit={(event) => {
-          doFetch(`https://hn.algolia.com/api/v1/search?query=${query}`);
-          
-          event.preventDefault();
-      }}>
-        <input
-          type="text"
-          value={query}
-          onChange={event => setQuery(event.target.value)}
-        />
-        <button type="submit">Search</button> 
-      </form>
-
-      {isError && <div>Something went wrong ...</div>}
-
-      {isLoading ? (
-        <div>Loading ...</div>
-      ) : (
-        <ul>
-          {data.hits.map(item => (
-            <li key={item.objectID}>
-              <a href={item.url}>{item.title}</a>
-            </li>
-          ))}
-        </ul>
-      )}
+      <SignIn/>
     </Fragment>
   );
 }
